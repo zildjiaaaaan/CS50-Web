fe57451d7507073e3eab2840ef69e01814398938
zildjiaaaaan <zildjian.m.18@gmail.com>
2023-04-22 12:08:00 +0800

    Add index and greet function

--- lecture3/hello/views.py
+++ lecture3/hello/views.py
@@ -3,4 +3,12 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world!")
+    # return HttpResponse("Hello, world!")
+    return render(request, "hello/index.html")    
+
+def greet(request, name):
+    # return HttpResponse(f"Hello, {name}")
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+    })
+    
