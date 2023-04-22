4295e01925b112562564b5aa4d4bd909c98c8dd6
zildjiaaaaan <zildjian.m.18@gmail.com>
2023-04-23 07:30:07 +0800

    Add validation in form

--- lecture3/tasks/views.py
+++ lecture3/tasks/views.py
@@ -5,6 +5,7 @@ tasks = ["foo", "bar", "baz"]
 
 class NewTaskForm(forms.Form):
     task = forms.CharField(label='New Task')
+    priority = forms.IntegerField(label='Priority', min_value=1, max_value=5)
 
 # Create your views here.
 def index(request):
@@ -13,6 +14,16 @@ def index(request):
     })
 
 def add(request):
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            tasks.append(task)
+        else:
+            return render(request, "tasks/add.html", {
+                'form': form
+            })
+        
     return render(request, "tasks/add.html", {
         'form': NewTaskForm()
     })
