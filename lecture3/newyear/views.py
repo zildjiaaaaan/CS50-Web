8c233618cebed9e259d16caf2bf4f83fc4c74242
zildjiaaaaan <zildjian.m.18@gmail.com>
2023-04-22 12:29:42 +0800

    Complete index function

--- lecture3/newyear/views.py
+++ lecture3/newyear/views.py
@@ -1,3 +1,9 @@
+from datetime import datetime
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    now = datetime.now()
+    return render(request, "newyear/index.html", {
+        "newyear": now.month == 1 and now.day == 1
+    })
