0a8315c73161a8fe0ceb75db64ae9b0c2e581692
zildjiaaaaan <zildjian.m.18@gmail.com>
2023-04-23 07:35:55 +0800

    Add user redirection after POST

--- lecture3/tasks/views.py
+++ lecture3/tasks/views.py
@@ -1,11 +1,12 @@
 from django import forms
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 tasks = ["foo", "bar", "baz"]
 
 class NewTaskForm(forms.Form):
     task = forms.CharField(label='New Task')
-    priority = forms.IntegerField(label='Priority', min_value=1, max_value=5)
 
 # Create your views here.
 def index(request):
@@ -19,6 +20,7 @@ def add(request):
         if form.is_valid():
             task = form.cleaned_data["task"]
             tasks.append(task)
+            return HttpResponseRedirect(reverse("tasks:index"))
         else:
             return render(request, "tasks/add.html", {
                 'form': form
