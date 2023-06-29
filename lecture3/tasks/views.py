27076af993274d9b187c5c6d61e8fd243ac4e1b8
zildjiaaaaan <zildjian.m.18@gmail.com>
2023-06-29 19:47:27 +0800

    Use layout.html

--- lecture3/tasks/views.py
+++ lecture3/tasks/views.py
@@ -1,4 +1,19 @@
+from django import forms
 from django.http import HttpResponse
 from django.shortcuts import render
 
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+
+tasks = ["foo", "bar", "baz"]
 # Create your views here.
+def index(request):
+    return render(request, "tasks/index.html", {
+        "tasks": tasks
+    })
+
+# Add a new task:
+def add(request):
+    return render(request, "tasks/add.html", {
+        "form": NewTaskForm()
+    })
