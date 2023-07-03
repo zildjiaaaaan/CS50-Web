a0e3ffcc0969fb04f58c7627f4f5f5524d02d618
zildjiaaaaan <zildjian.m.18@gmail.com>
2023-07-03 20:27:14 +0800

    Add add function

--- lecture3/tasks/views.py
+++ lecture3/tasks/views.py
@@ -14,6 +14,16 @@ def index(request):
 
 # Add a new task:
 def add(request):
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            tasks.append(task)
+        else:
+            return render(request, 'tasks/add.html', {
+                "form": form
+            })
+
     return render(request, "tasks/add.html", {
         "form": NewTaskForm()
     })
