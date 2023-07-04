47771e65dcd98a149af2e115bd153ec92b1ecf54
zildjiaaaaan <zildjian.m.18@gmail.com>
2023-07-04 22:41:42 +0800

    Add new tasks app

--- lecture3/tasks/views.py
+++ lecture3/tasks/views.py
@@ -3,30 +3,32 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-tasks = []
+# Create your views here.
+# tasks = []
 
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
 
-# Create your views here.
 def index(request):
+    # request.session.clear()
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
     return render(request, "tasks/index.html", {
-        "tasks": tasks
+        "tasks": request.session["tasks"]
     })
 
-# Add a new task:
 def add(request):
     if request.method == "POST":
         form = NewTaskForm(request.POST)
         if form.is_valid():
             task = form.cleaned_data["task"]
-            tasks.append(task)
+            request.session["tasks"] += [task]
             return HttpResponseRedirect(reverse("tasks:index"))
         else:
-            return render(request, 'tasks/add.html', {
-                "form": form
+            return render(request, "tasks/add.html", {
+                "forms": form
             })
 
     return render(request, "tasks/add.html", {
-        "form": NewTaskForm()
+        "forms": NewTaskForm()
     })
